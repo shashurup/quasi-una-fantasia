590f9e940b87263ec007a3913109869ae3c81c8e
Georgy Kibardin <georgy@kibardin.name>
2023-03-24 18:33:52 +0300

    Convert source data CRS automatically

--- src/rackushka/ol.cljs
+++ src/rackushka/ol.cljs
@@ -3,23 +3,25 @@
             [goog.dom :as gdom]
             [cljsjs.openlayers]))
 
-(defn make-source [subj]
+(defn make-source [subj proj]
   (js/ol.source.Vector.
    (clj->js {:features
              (mapv #(.readFeature (js/ol.format.WKB.)
-                                  (str (:geometry %))) subj)})))
+                                  (:geometry %)
+                                  #js {:featureProjection proj}) subj)})))
 
-(defn create-map-control [target src]
-  (let [view (js/ol.View. (clj->js {:center [0 0] :zoom 1}))
+(defn create-map-control [target geodata]
+  (let [view (js/ol.View. (clj->js {:padding [32 32 32 32]}))
         osm-tiles (js/ol.layer.Tile. (clj->js {:source (js/ol.source.OSM.)}))
+        src (make-source geodata (.getProjection view))
         subj-layer (js/ol.layer.Vector. (clj->js {:source src}))]
     (js/ol.Map. (clj->js {:layers [osm-tiles subj-layer]
                           :target target
                           :view view}))
-    (.fit view (.getExtent src) (clj->js {:padding [32 32 32 32]}))))
+    (.fit view (.getExtent src))))
 
 (defmethod c/render :geodata [subj]
   (fn [target]
     (let [c (gdom/createDom "div" #js {:style "width: 100%; height: 40vh"})]
       (gdom/appendChild target c)
-      (create-map-control c (make-source subj)))))
+      (create-map-control c subj))))
