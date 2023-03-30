97d70929fad08958a73468f949c889827908965d
Georgy Kibardin <georgy@kibardin.name>
2023-03-30 11:16:13 +0300

    Feature label support

--- src/rackushka/ol.cljs
+++ src/rackushka/ol.cljs
@@ -3,18 +3,30 @@
             [goog.dom :as gdom]
             [cljsjs.openlayers]))
 
+(defn read-wkb [subj proj]
+  (.readFeature (js/ol.format.WKB.) subj #js {:featureProjection proj}))
+
+(defn set-props [subj props]
+  (.setProperties subj (clj->js props))
+  subj)
+
+(defn make-style [feature]
+  (js/ol.style.Style.
+   #js {:fill (js/ol.style.Fill. #js {:color "rgba(255,255,255,0.6)"})
+        :stroke (js/ol.style.Stroke. #js {:color "#00f"})
+        :text (js/ol.style.Text. #js {:text (.get feature "label")})}))
+
 (defn make-source [subj proj]
   (js/ol.source.Vector.
    (clj->js {:features
-             (mapv #(.readFeature (js/ol.format.WKB.)
-                                  (:geometry %)
-                                  #js {:featureProjection proj}) subj)})))
+             (mapv #(set-props (read-wkb (:geometry %) proj)
+                               (dissoc % :geometry)) subj)})))
 
 (defn create-map-control [target geodata]
   (let [view (js/ol.View. (clj->js {:padding [32 32 32 32]}))
         osm-tiles (js/ol.layer.Tile. (clj->js {:source (js/ol.source.OSM.)}))
         src (make-source geodata (.getProjection view))
-        subj-layer (js/ol.layer.Vector. (clj->js {:source src}))]
+        subj-layer (js/ol.layer.Vector. (clj->js {:source src :style make-style}))]
     (js/ol.Map. (clj->js {:layers [osm-tiles subj-layer]
                           :target target
                           :view view}))
