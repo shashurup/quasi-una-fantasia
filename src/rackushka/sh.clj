29468ff8d6ed10e98cdc4894b0ffaca32d51a700
Georgy Kibardin <georgy@kibardin.name>
2023-05-01 08:43:38 +0300

    ! can have an input now

--- src/rackushka/sh.clj
+++ src/rackushka/sh.clj
@@ -2,27 +2,59 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
             [rackushka.fs :as fs])
-  (:import [java.lang ProcessBuilder]))
+  (:import [java.lang ProcessBuilder ProcessBuilder$Redirect]))
 
 ;; clojure.java.shell/sh always create a pipe for stdin
 ;; this ins't always what we need
 ;; for instance, ag thinks it needs to search stdin in this case
 ;; so here goes our own implementation
 
-(defn- start-process [cmd dir]
+(defn- start-process [cmd dir in]
   (let [p (-> (ProcessBuilder. (into-array String cmd))
               (.directory (io/as-file dir))
-              (.redirectInput (io/as-file "/dev/null"))
+              (.redirectInput (if in
+                                ProcessBuilder$Redirect/PIPE
+                                (io/as-file "/dev/null")))
               .start)]
-    {:out (io/reader (.getInputStream p))
+    {:in (.getOutputStream p)
+     :out (io/reader (.getInputStream p))
      :err (io/reader (.getErrorStream p))
      :wait #(.waitFor p)}))
 
-(defn ! [& args]
-  (let [args (if (> (count args) 1)
-               args
-               (remove empty? (s/split (first args) #"\s")))]
-    (let [{:keys [out err wait]} (start-process args fs/*cwd*)]
+(defn !
+  "Launches a subprocess.
+  Subprocess is defined either by a single string:
+
+    (! \"ls -al\")
+
+  or by a list of strings:
+
+    (! \"ls\" \"-al\")
+  
+  Standard input may also be specified:
+
+    (! \"sort\" :input [\"def\" \"ghi\" \"abc\"])
+
+  it could be a list of string or anything that can be
+  copied with clojure.java.io/copy.
+  Another option is :dir for a directory to use.
+  Return value is a process output as a list of strings.
+  Exception is thrown when exit code is non zero."
+  [& args]
+  (let [[cmd {:keys [input dir]}] (split-with string? args)
+        cmd (if (> (count cmd) 1)
+               cmd
+               (remove empty? (s/split (first cmd) #"\s")))]
+    (let [{:keys [in out err wait]} (start-process cmd
+                                                   (or dir fs/*cwd*)
+                                                   input)]
+      (when input
+        (future
+          (let [input (if (coll? input)
+                        (s/join "\n" input)
+                        input)]
+            (with-open [in' in]
+              (io/copy input in')))))
       (let [output (vec (line-seq out))
             error (future (slurp err))
             exit (wait)]
