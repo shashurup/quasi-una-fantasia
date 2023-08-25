499bc7772bf7ab6425d2fde930bacff4c10a76f6
Georgy Kibardin <georgy@kibardin.name>
2023-08-25 10:19:51 +0300

    Bound histogram to a grid

--- src/shashurup/quf/chart.clj
+++ src/shashurup/quf/chart.clj
@@ -1,4 +1,21 @@
-(ns shashurup.quf.chart)
+(ns shashurup.quf.chart
+  (:require [clojure.math :as math]
+            [shashurup.quf.events :as events]))
+
+(def grid [10 15 20 25 30 40 50 60 70 80 90 100])
+
+(defn- snap-to-grid
+  ([subj] (snap-to-grid subj 0))
+  ([subj n]
+    (let [m (* subj (math/pow 10 (- n)))]
+      (cond
+        (> m (last grid)) (snap-to-grid subj (inc n))
+        (< m (first grid)) (snap-to-grid subj (dec n))
+        :else (->> grid
+                   (map #(vector % (abs (- % m))))
+                   (sort-by second)
+                   ffirst
+                   (* (math/pow 10 n)))))))
 
 (defn normalize [data]
   (for [x data]
@@ -17,13 +34,16 @@
 (defn scatter [data] (with-meta data {:shashurup.quf/hint [:chart :scatter]}))
 
 (defn histogram
-  ([data] (histogram data 11))
+  ([data] (histogram 11 data))
   ([n data]
-   (let [from (apply min data)
-         step (/ (- (apply max data) from) n)]
+   (let [first-val (apply min data)
+         step (snap-to-grid (/ (- (apply max data) first-val) n))
+         start (* (quot first-val step) step)]
      (->> (reduce (fn [a v]
-                    (let [k (quot (- v from) step)]
+                    (let [k (quot (- v start) step)]
                       (update a k #(inc (or % 0))))) {} data)
-          (map (fn [[k v]] [(+ from (* step k)) v]))
+          (map (fn [[k v]] [(+ start (* step k)) v]))
           (sort-by first)
           bar))))
+
+(events/push {:type :require :ns "shashurup.quf.chartjs"})
