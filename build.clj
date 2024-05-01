2db21c4dd5573d9ae1636989c808ee98deef5f34
Georgy Kibardin <georgy@kibardin.name>
2024-05-01 09:33:06 +0300

    Release 0.14

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.pprint :refer [pprint]]))
 
 (def lib 'shashurup/quasi-una-fantasia)
-(def version "0.13")
+(def version "0.14")
 
 (def target "target")
 (def slim-jar-dir (format "%s/slim-jar" target))
