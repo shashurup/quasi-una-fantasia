2dc67d1416b5b460082be6cb2bd7f3c6134d497a
Georgy Kibardin <georgy@kibardin.name>
2025-09-07 16:02:24 +0300

    Fix middleware load order
    Seems to be :requires and :expects are mixed up

--- src/shashurup/quf/vars.clj
+++ src/shashurup/quf/vars.clj
@@ -1,7 +1,6 @@
 (ns shashurup.quf.vars
   (:require [nrepl.middleware :as mwre]
-            [nrepl.misc :as m]
-            [nrepl.transport :as t]))
+            [nrepl.middleware.session :refer [session]]))
 
 (def ^:dynamic *term-dimensions* [80 24])
 
@@ -19,13 +18,12 @@
 
 (defn wrap-update-vars [h]
   (fn [{:keys [op session var-updates transport] :as msg}]
-    (println op (class session) var-updates h)
     (when (and (= op "eval") var-updates)
         (swap! session apply-updates (read-string var-updates)))
     (h msg)))
 
 (mwre/set-descriptor! #'wrap-update-vars
-                      {:requires #{"eval"}
-                       :expects #{'mwre.session}
+                      {:expects #{"eval"}
+                       :requires #{#'session}
                        :handles {"update-vars" {:doc "Updates session vars bindings"
                                                 :requires {"updates" "list of updates/new values"}}}})
