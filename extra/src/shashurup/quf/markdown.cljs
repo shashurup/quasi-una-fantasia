b25be8a59c680fc206bd514ef830d777048c82f6
Georgy Kibardin <georgy@kibardin.name>
2026-04-18 16:35:45 +0300

    Highlight code and draw seamless tables in Markdown

--- extra/src/shashurup/quf/markdown.cljs
+++ extra/src/shashurup/quf/markdown.cljs
@@ -1,10 +1,40 @@
 (ns shashurup.quf.markdown
   (:require [clojure.string :as s]
             [shashurup.quf.render :refer [render hint-args]]
-            [marked]))
+            [marked]
+            [hljs]))
 
 (defn parse [subj]
   (.parse js/marked subj))
+
+(defn render-code [subj]
+  (str "<pre><code>"
+       (.-value (hljs/highlightAuto (.-text subj)
+                                    (to-array [(.-lang subj)])))
+       "</code></pre>"))
+
+(defn render-table [subj]
+  (let [rndr #(.parseInline js/marked (.-text %))]
+    (str "<table class=quf >"
+         "<thead><tr>"
+         (apply str (for [h (.-header subj)]
+                      (str "<th class=quf>" (rndr h) "</th>")))
+         "</tr></thead>"
+         "<tbody>"
+         (apply str (for [row (.-rows subj)]
+                      (apply str
+                             "<tr>"
+                             (apply str (for [cell row]
+                                          (str "<td class=quf>"
+                                               (rndr cell)
+                                               "</td>")))
+                             "</tr>"
+                             )))
+         "</tbody>"
+         "</table>")))
+
+(.use js/marked (clj->js {:renderer {:code render-code
+                                     :table render-table}}))
 
 (defn- from-raw-html [subj]
   (let [templ (.createElement js/document "template")]
