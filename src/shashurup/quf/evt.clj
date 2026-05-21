9074fa466b3a48c93a6aca51f836ea77128283c9
Georgy Kibardin <georgy@kibardin.name>
2026-05-21 13:59:45 +0300

    Event protocol, cancel from client

--- src/shashurup/quf/evt.clj
+++ src/shashurup/quf/evt.clj
@@ -4,35 +4,41 @@
             [nrepl.middleware.session :refer [session]]
             [nrepl.transport :as t]))
 
+(defonce cancel-fns (atom {}))
+
 (defn wrap-event-reporter [h]
-  (fn [{:keys [id session op transport] :as msg}]
-    (when (= op "eval")
-      (let [session-id (:id (meta session))
-            re (fn [subj]
-                 (t/send transport
-                         {:id id
-                          :session session-id
-                          :event (binding [*print-meta* true]
-                                   (pr-str subj))})
-                 nil)
-            ev-start (fn []
-                       (t/send transport
-                               {:id id
-                                :session session-id
-                                :status #{:expect-background-events}}))
-            ev-stop (fn []
-                      (t/send transport
-                              {:id id
-                               :session session-id
-                               :status #{:no-more-background-events}}))]
-        (swap! session assoc
-               #'r/report-event re
-               #'r/expect-background-events ev-start
-               #'r/no-more-background-events ev-stop)))
-    (h msg)))
+  (fn [{:keys [id session op] :as msg}]
+    (let [session-id (:id (meta session))]
+      (condp = op
+        "eval" (let [re (fn [subj]
+                          (t/respond-to msg :event (binding [*print-meta* true]
+                                                     (pr-str subj)))
+                          nil)
+                     ev-start (fn [cancel]
+                                (swap! cancel-fns
+                                       assoc-in [session-id id] cancel)
+                                (t/respond-to msg
+                                              :status
+                                              #{:expect-background-events})
+                                nil)
+                     ev-stop (fn []
+                               (t/respond-to msg
+                                             :status
+                                             #{:no-more-background-events})
+                               nil)]
+                 (swap! session assoc
+                        #'r/report-event re
+                        #'r/expect-background-events ev-start
+                        #'r/no-more-background-events ev-stop)
+                 (h msg))
+        "cancel-events" (if-let [cancel (get-in @cancel-fns [session-id id])]
+                          (do (cancel)
+                              (t/respond-to msg :status #{:done}))
+                          (t/respond-to msg :status #{:done :error :not-found}))
+        (h msg)))))
 
 (mwre/set-descriptor! #'wrap-event-reporter
                       {:expects #{"eval"}
                        :requires #{#'session}
-                       :handles {"set-event-reporting-callback"
-                                 {:doc "Sets up report-event callback"}}})
+                       :handles {"cancel-events"
+                                 {:doc "Cancel events sent by a producer"}}})
--- src/shashurup/quf/response.clj
+++ src/shashurup/quf/response.clj
@@ -14,13 +14,15 @@
     (pr (hint subj hint_))
     (flush)))
 
-(def ^:dynamic expect-background-events (fn []))
+(def ^:dynamic expect-background-events (fn [cancel]))
 
 (def ^:dynamic no-more-background-events (fn []))
 
 ;; Fallback event reporting function
 ;; Overriden in evt middleware
 (def ^:dynamic report-event pr)
+
+(def ^:dynamic session-id nil)
 
 (defn report-progress
   ([message] (report-progress message nil nil))
@@ -36,3 +38,23 @@
        (map meta)
        (map :shashurup.quf/client-module)
        (filter identity)))
+
+(defn track
+  ([subj] (track subj identity))
+  ([subj conv]
+   (let [key (str "event-handler-" (random-uuid))
+         re report-event
+         nmbe no-more-background-events
+         handler (fn [key subj _ new-state]
+                   (try
+                     (re {:type :value
+                          :value (conv new-state)})
+                     (catch Exception ex
+                       (when (= (:reason (ex-data ex)) :timeout)
+                         (remove-watch subj key)))))
+         untrack (fn []
+                   (nmbe)
+                   (remove-watch subj key))]
+     (expect-background-events untrack)
+     (add-watch subj key handler)
+     (conv @subj))))
